b948f90656330721702bc087bd16284126b84852
Sudesh Kharel <sudeshkharel15@gmail.com>
2024-06-27 14:36:15 +0545

    added folders

---
+++ Firebase/app/src/main/java/com/example/laxman/Signup.java
@@ -0,0 +1,32 @@
+package com.example.laxman;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Signup extends AppCompatActivity {
+EditText email_txt,password_txt,cpassword_txt;
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        email_txt=findViewById(R.id.text_email);
+        password_txt=findViewById(R.id.txt_password);
+        cpassword_txt=findViewById(R.id.txt_cpassword);
+        btn=findViewById(R.id.btn_signup);
+
+    }
+}
